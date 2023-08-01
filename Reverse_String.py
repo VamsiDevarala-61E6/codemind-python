cacf646a026dce92f0f158b3981dad89bb8171fa
VAMSI DEVARALA <119839709+VamsiDevarala-61E6@users.noreply.github.com>
2023-08-01 18:42:40 +0530

    s=input() k="" v=[] for i in s:     if i!=" ":         k+=i     elif i==" ":         v.append(k)         k="" v.append(k) v.reverse() for j in v:     print(j,end=" ")

--- Reverse_String.py
+++ Reverse_String.py
@@ -1,4 +1,13 @@
-def rev(s):
-    return s[::-1]
 s=input()
-print(rev(s))
+k=""
+v=[]
+for i in s:
+    if i!=" ":
+        k+=i
+    elif i==" ":
+        v.append(k)
+        k=""
+v.append(k)
+v.reverse()
+for j in v:
+    print(j,end=" ")
