655172486156e3fdde95c22a4d69647253ddfe83
VAMSI DEVARALA <119839709+VamsiDevarala-61E6@users.noreply.github.com>
2023-08-04 19:28:51 +0530

    def rev(n):     r=0     while(n!=0):         r=r*10+n%10         n=n//10     return r n=int(input()) if n>0:     print(rev(n)) else:     x=-1*n     t=rev(x)     print(-1*t)

--- Reverse_Integer.py
+++ Reverse_Integer.py
@@ -1,19 +1,13 @@
+def rev(n):
+    r=0
+    while(n!=0):
+        r=r*10+n%10
+        n=n//10
+    return r
 n=int(input())
 if n>0:
-    t=n
-    r=0
-    while n!=0:
-        x=n%10
-        r=(r*10)+x
-        n=n//10
-    print(r)
+    print(rev(n))
 else:
-    n=-1*n
-    t=n
-    r=0
-    while n!=0:
-        x=n%10
-        r=(r*10)+x
-        n=n//10
-    print(-r)
-    
+    x=-1*n
+    t=rev(x)
+    print(-1*t)
